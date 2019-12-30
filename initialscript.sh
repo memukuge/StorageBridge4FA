83e9eb66fa887fbe70d36c70db44d09cd6ed18e9
memukuge <memukuge@gmail.com>
2019-12-31 00:30:32 +0900

    issues may be fixed
    issueにある、カード切り替え時の動作を改善できたはず。あと、一時ファイルをtmpに置いた

--- initialscript.sh
+++ initialscript.sh
@@ -5,13 +5,20 @@ export PATH=$PATH:/usr/local/bin
 
 #check whether card installed
 if ls -l /dev/disk/by-uuid | grep 'sda1' > /dev/null  ; then
+
+	dsksizGB=$(fdisk -l /dev/sda | grep "Disk /dev/sda" | cut -f 3 -d " ")
+	echo $dsksizGB > /tmp/cardsize
+	bootsec=$(hexdump -n 4 -s 454 -e '"%08d"' /dev/sda) #get 1st partition boot sector.
+	bootByteOffset=$(expr $bootsec \* 512)              #get Boot Sector offset byte from disk head
+	echo $bootByteOffset >/tmp/bootbyteoffset
+
 	#card already installed
 	/sbin/modprobe g_mass_storage file=/dev/sda removable=1 stall=0
 	echo 2 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role
-	echo "1" > ./isinserted #indicate card is inserted
+	echo "1" > /tmp/isinserted #indicate card is inserted
 else
 	#card is not installed
-	echo "0" > ./isinserted #indicate card isn't inserted
+	echo "0" > /tmp/isinserted #indicate card isn't inserted
 fi
 
 #activates cyclic scripts.
--- removecheck.sh
+++ removecheck.sh
@@ -5,23 +5,30 @@
 
 interval=1
 
-while true ; do
-  inserted=`cat ./isinserted`
-  if ls -l /dev/disk/by-uuid | grep 'sda1' > /dev/null  ; then
-    if [ "0" = ${inserted} ] ; then # card is inserted
+while true; do
+  inserted=$(cat /tmp/isinserted)
+  if ls -l /dev/disk/by-uuid | grep 'sda1' >/dev/null; then
+    if [ "0" = ${inserted} ]; then # card is inserted
+
+      dsksizGB=$(fdisk -l /dev/sda | grep "Disk /dev/sda" | cut -f 3 -d " ")
+      echo $dsksizGB > /tmp/cardsize
+      bootsec=$(hexdump -n 4 -s 454 -e '"%08d"' /dev/sda) #get 1st partition boot sector.
+      bootByteOffset=$(expr $bootsec \* 512)              #get Boot Sector offset byte from disk head
+      echo $bootByteOffset > /tmp/bootbyteoffset
+
       modprobe g_mass_storage file=/dev/sda removable=1 stall=0
-      echo 2 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role
+      echo 2 >/sys/bus/platform/devices/sunxi_usb_udc/otg_role
       #echo "inserted"
-      echo "1" > ./isinserted
+      echo "1" > /tmp/isinserted
     fi
   else
-    if [ "1" = ${inserted} ] ; then
-      echo 0 > /sys/bus/platform/devices/sunxi_usb_udc/otg_role
+    if [ "1" = ${inserted} ]; then
+      echo 0 >/sys/bus/platform/devices/sunxi_usb_udc/otg_role
       rmmod g_mass_storage
       #echo "removed"
-      echo "0" > ./isinserted
+      echo "0" > /tmp/isinserted
     fi
   fi #else
   sleep $interval
-#echo "removecheck"
+  #echo "removecheck"
 done #while
